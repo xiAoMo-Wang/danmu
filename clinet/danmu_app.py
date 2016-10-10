32d3e846fae2811a8b0a56d19e30bee070f763d6
ZHU ZICHEN <zhuzichenchn@qq.com>
2016-10-10 23:11:30 +0800

    加入了部分注释 still WIP DanmuApp类__init__的注释仍然不全 两个类的类文档WIP

--- clinet/danmu_app.py
+++ clinet/danmu_app.py
@@ -4,8 +4,8 @@ from PyQt4 import QtGui, QtCore
 
 import danmu_window
 
-class DanmuApp(QtGui.QWidget):
 
+class DanmuApp(QtGui.QWidget):
     def __init__(self, *args, **kwargs):
         self.danmu_windows = list()
 
@@ -13,11 +13,11 @@ class DanmuApp(QtGui.QWidget):
 
         self.setWindowTitle('danmu')
 
-        self.init_windows()
+        self.init_window()
 
-        mainBtn = QtGui.QPushButton('main button', self)
+        main_btn = QtGui.QPushButton('main button', self)
 
-        self.connect(mainBtn, QtCore.SIGNAL('clicked()'), self.buttonClicked)
+        self.connect(main_btn, QtCore.SIGNAL('clicked()'), self.button_clicked)
 
         desktop = QtGui.QDesktopWidget()
         screen_center = desktop.screenGeometry(desktop.primaryScreen()).center()
@@ -27,15 +27,27 @@ class DanmuApp(QtGui.QWidget):
 
         self.show()
 
-    def buttonClicked(self):
+    def button_clicked(self):
+        """
+        按钮被点击的信号槽 打印出信号发出者的名称
+        :return:
+        """
         print self.sender().text()
 
-    def init_windows(self):
-
+    def init_window(self):
+        """
+        初始化屏幕窗口
+        :return:
+        为每个屏幕创建一个弹幕窗口 用于弹幕的显示 并将创建的Widget对象加入列表
+        """
+        # 获取QDesktopWidget
         desktop = QtGui.QDesktopWidget()
+        # 获取屏幕数量
         screen_count = desktop.screenCount()
+        # 遍历每个屏幕并为其创建DanmuWindow对象
         for i in range(screen_count):
             w = danmu_window.DanmuWindow(i, self)
             self.danmu_windows.append(w)
+        # 调试用 打印出每个屏幕对应的窗口对象
         for w in self.danmu_windows:
             print w
--- clinet/danmu_window.py
+++ clinet/danmu_window.py
@@ -5,14 +5,24 @@ from PyQt4 import QtGui, Qt
 
 class DanmuWindow(QtGui.QWidget):
     def __init__(self, screen_number, parent, *args, **kwargs):
+        """
+        DanmuWindow初始化函数
+        :param screen_number: int 所对应的屏幕编号
+        :param parent: QWidget 父对象 DanmuApp
+        为对应屏幕创建DanmuWindow
+        窗口顶置 透明 直接透过鼠标与键盘操作到下一层 无边框
+        """
+        # 创建父类对象
         QtGui.QWidget.__init__(self, *args, **kwargs)
-
+        # 设置父对象
         self.setParent(parent)
 
+        # 获得屏幕编号对应的屏幕大小信息
         desktop = QtGui.QDesktopWidget()
         geo = desktop.screenGeometry(screen_number)
         # print geo.topLeft()
 
+        # 设置窗口为全屏 高和宽均为屏幕高和宽
         self.resize(geo.width(), geo.height())
         self.setWindowFlags(Qt.Qt.X11BypassWindowManagerHint
                             | Qt.Qt.WindowStaysOnTopHint
@@ -24,9 +34,11 @@ class DanmuWindow(QtGui.QWidget):
         self.setAttribute(Qt.Qt.WA_TransparentForMouseEvents, True)
         self.setStyleSheet('background: transparent')
 
+        #将窗口移动到屏幕左上角
         self.move(geo.topLeft())
 
+        #显示窗口
         self.show()
+
+        # 调试用
         print self.testAttribute(Qt.Qt.WA_Disabled)
-
-
